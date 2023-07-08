cb3514c4d52fe9a077ff2b47149640ad216e1ab6
JulianaGFAlmeida <julianagfalmeida@gmail.com>
2023-07-08 01:10:08 -0300

    Created a timer that activaated when a certain amount of itens is collected
    Then, it runs out and set the itens amount back to 0. Also, moved the ElevatorBehaviour Script to the correct folder

---
+++ Assets/Scripts/CounterBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float totalTime = 60f; 
+    private float currentTime=0;
+    private Text countdownText;
+    private bool isTimerRunning = false;
+
+    private void Start()
+    {
+        countdownText = GetComponent<Text>();
+       
+    }
+
+    private void Update()
+    {
+        if (isTimerRunning) { UpdateTimer(); }
+    }
+
+    void UpdateTimer()
+    {
+
+        currentTime -= Time.deltaTime;
+
+        // Display the time remaining in the UI text element
+        countdownText.text = currentTime.ToString("0");
+
+        // Check if the countdown has reached zero
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            isTimerRunning = false;
+            // Perform any actions you need when the timer reaches zero
+        }
+    }
+    public void StartTimer()
+    {
+        currentTime = totalTime;
+        isTimerRunning = true;
+    }
+}
---
+++ Assets/Scripts/DemonTransformerManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonTransformerManager : MonoBehaviour
+{
+    [SerializeField]
+    [Range(5,10)]
+    private int amountNeeded=5;
+    private int amountCollected;
+    [SerializeField]
+    private CounterBehaviour cb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        amountCollected = 0;
+    }
+
+    // Update is called once per frame
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Transformer"))
+        {
+            amountCollected++;
+            Debug.Log(amountCollected);
+            Destroy(collision.gameObject);
+            if(CheckIfEnough())
+            {
+                cb.StartTimer();
+            }
+        }
+    }
+    private bool CheckIfEnough()
+    {
+        return amountCollected >= amountNeeded;
+    }
+
+    public void ResetAmountCollected()
+    {
+        amountCollected = 0;
+    }
+}
